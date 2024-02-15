2535d1ac52a8aa579952cfc3dd3fa4f8b3af89ad
Kseniya <xeniyaoz45@gmail.com>
2024-02-15 17:41:29 +0200

    Start working with update function

--- CU_menu_window.py
+++ CU_menu_window.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtCore
 from datetime import datetime
 
+import TaskManager
 import cu_menu
 import db_helper
 
@@ -18,6 +19,8 @@ class CU_Menu(QMainWindow):
         self.ui.setupUi(self)
 
         self.db = db_helper.DB_helper()
+
+        self.mm = None
 
         self.ui.discard_changes_Btn.clicked.connect(self.back)
         self.ui.save_changes_Btn.clicked.connect(self.create_task)
@@ -36,4 +39,23 @@ class CU_Menu(QMainWindow):
         status = self.db.create_new_task(data)
         self.got_status.emit(status)
         self.close()
+
+    def start_update_task(self):
+
+        self.mm = TaskManager.TaskManager()
+        self.mm.got_id.connect(self.update_task)
+
+    def update_task(self, id):
+        data = {
+            'id': id,
+            'task_name': self.ui.task_name_field.text(),
+            'task_desc': self.ui.task_description_field.toPlainText(),
+            'start_date': datetime.now().date(),
+            'exp_date': self.ui.expiration_date_spinner.date().toPyDate(),
+            'status': self.ui.task_status_list.selectedItems()[0]
+        }
+        status = self.db.update_checked_task(data)
+        self.got_status.emit(status)
+        self.close()
+
 
--- TaskManager.py
+++ TaskManager.py
@@ -9,6 +9,7 @@ import main_menu
 
 
 class TaskManager(QMainWindow):
+
     got_id = QtCore.pyqtSignal(int)
 
     def __init__(self):
@@ -31,6 +32,9 @@ class TaskManager(QMainWindow):
         self.ui.actionUnselect_all_actual_tasks.triggered.connect(self.unselect_all_actual_tasks)
         self.ui.actionUnselect_All_complete_tasks.triggered.connect(self.unselect_all_complete_tasks)
         self.ui.actionAdd_new.triggered.connect(self.start_task_creation)
+        self.ui.actionUpdate_selected.triggered.connect(self.start_task_updation)
+
+        self.ui.create_new_task_Btn.clicked.connect(self.start_task_creation)
 
     def init_db(self):
         files = os.listdir()
@@ -61,7 +65,28 @@ class TaskManager(QMainWindow):
         self.cu_menu.show()
 
     def create_task(self, status):
-        self.ui.statusbar.setStatusTip(status)
+        self.ui.statusbar.showMessage(status)
+        self.load_uncompleted_tasks()
+
+    def start_task_updation(self):
+        if len(self.ui.actual_tasks_list.selectedItems()) != 0:
+            selected_task_id = self.ui.actual_tasks_list.currentRow()
+            selected_task = self.uncompleted_tasks[selected_task_id]
+
+            self.cu_menu = CU_menu_window.CU_Menu()
+            self.cu_menu.ui.task_name_field.setText(selected_task.name)
+            self.cu_menu.ui.task_description_field.setPlainText(selected_task.description)
+            self.cu_menu.ui.expiration_date_spinner.setDate(selected_task.expiration_date)
+
+            self.got_id.emit(selected_task.id)
+            self.cu_menu.got_status.connect(self.update_task)
+
+            self.cu_menu.show()
+        else:
+            self.ui.statusbar.showMessage('YOU NEED TO SELECT TASK (UNCOMPLETE) FOR UPDATING')
+
+    def update_task(self, status):
+        self.ui.statusbar.showMessage(status)
         self.load_uncompleted_tasks()
 
     def unselect_all_actual_tasks(self):
